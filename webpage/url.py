f232c2948e516d978a1fe60bdfd1d48403942ca2
Gautham <thabeatsz@gmail.com>
2017-01-30 05:20:05 +0530

    Add realted timeline

--- webpage/url.py
+++ webpage/url.py
@@ -5,10 +5,12 @@ from django.views.generic import TemplateView
 from .views import (
 
 	Timeline,
+	RelatedTimeline,
 	)
 
 urlpatterns = [
 	url(r'^$', Timeline,name="Timeline"),
+	url(r'(?P<Topic>[A-Za-z0-9]+)$',RelatedTimeline, name='RelatedTimeline'),
 	# url(r'^rank$', rank,name="ranks"),
 	# url(r'^$',index,name="index"),
 
--- webpage/views.py
+++ webpage/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 import requests
+from django.http import HttpResponseRedirect
 
 from .models import Article
 # Create your views here.
@@ -8,6 +9,9 @@ from .models import Article
 
 def Timeline(request):
 	SummaryList = []
+	query = request.GET.get("q")
+	if query :
+		return HttpResponseRedirect("/webpage/"+query)
 	for item in range (10):
 		Summary= requests.get('https://en.wikipedia.org/api/rest_v1/page/random/summary',verify=False)
 	
@@ -23,3 +27,18 @@ def Timeline(request):
 	# TempData = Temp.json()
 		# TempTitle = TempData["items"][0]["title"]
 		# Summary = requests.get('https://en.wikipedia.org/api/rest_v1/page/summary/' + TempTitle) 
+
+def RelatedTimeline(request,Topic):
+	#returns top 5 related to a topic
+	RelatedList = requests.get('https://en.wikipedia.org/api/rest_v1/page/related/'+ Topic,verify=False)
+	RelatedListData = RelatedList.json()
+
+	query = request.GET.get("q")
+	if query :
+		return HttpResponseRedirect("/webpage/"+query)
+
+
+	context={
+	"Data" : RelatedListData
+	}
+	return render(request,"relatedtimeline.html",context)
